40fd5b4acd3c63e60826ac138a13b2f9299aac41
Talon Poole <talon.poole@gmail.com>
2014-10-21 19:06:28 -0600

    add photo slider and description

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 (function(window) {
-  $('#omni-ghost').draggable()
+  var slider = new IdealImageSlider.Slider({
+    selector: '#slider',
+    height: 400, // Required but can be set by CSS
+    interval: 4000,
+  });
+  console.log('hello', slider);
+  slider.start();
 }(window))
